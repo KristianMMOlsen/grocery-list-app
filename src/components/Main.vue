ecbf9a3f23e4a647c943d438b5e8722e3da84f10
Kristian Olsen <33516139+KristianMMOlsen@users.noreply.github.com>
2021-02-26 23:06:42 +0100

    new design on the delete button

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -38,8 +38,8 @@
           >
             {{ item }}
             <!-- button that when clicked removes targeted items from the list by using the removeItem function -->
-            <span v-on:click="removeItem(index)" type="button" id="removeItem"
-              >
+            <button v-on:click="removeItem(index)" type="button" class="btn btn-danger" id="removeItem">
+            
               <!-- the svg is the icon for the trashcan on the remove button -->
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -56,7 +56,7 @@
                 preserveAspectRatio="xMidYMid meet"
                 viewBox="0 0 16 16"
               >
-                <g fill="#626262">
+                <g fill="#000">
                   <path
                     d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
                   />
@@ -65,7 +65,7 @@
                     d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
                   />
                 </g></svg
-            ></span>
+            ></button>
           </li>
         </ul>
       </div>
@@ -108,9 +108,9 @@ export default {
     //function that removes selected item from the list by using splice with index as identifier
     removeItem: function (index) {
       this.items.splice(index, 1);
-    },
-  },
-};
+    }
+  }
+}
 </script>
 <!-- css styling for this component only -->
 <style scoped>
@@ -135,10 +135,10 @@ li:nth-child(even) {
 /* styling for the button that removes items from the list */
 #removeItem {
   float: right;
-  color: black;
 }
 /* aligns the text on the list items to the left */
 .list-group-item {
   text-align: left;
+  font-size: 25px;
 }
 </style>
