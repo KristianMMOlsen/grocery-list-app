a5b9dab97c26b927a8b631f8140ca27d5df03f4b
Kristian Olsen <33516139+KristianMMOlsen@users.noreply.github.com>
2021-03-03 15:42:21 +0100

    Added responsive breakpoints, fixed margin issues

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,15 @@
 <template>
   <!-- container which wraps around the whole application -->
-  <div class="container">
+  <div class="container mx-0 px-0">
     <!-- row for the title -->
-    <div class="row">
+    <div class="row mx-0">
       <div class="col-12">
         <h1>GROCERY LISTS</h1>
       </div>
     </div>
     <!-- row for the textfield for adding items -->
-    <div class="row justify-content-center">
-      <div class="col-6 pr-0">
+    <div class="row justify-content-center mx-0">
+      <div class="col-7 col-md-10 col-lg-10 pr-0">
         <div class="form-group">
           <input
             v-model="newItem"
@@ -19,7 +19,7 @@
           />
         </div>
       </div>
-      <div class="col-4 px-0">
+      <div class="col-5 col-md-2 col-lg-2 px-0">
         <!-- button that adds items to the list by using the addItem function -->
         <button v-on:click="addItem" class="btn btn-success">
           Add to list
@@ -27,8 +27,8 @@
       </div>
     </div>
     <!-- row for the text area listing items -->
-    <div class="row justify-content-center mt-3">
-      <div class="col-10">
+    <div class="row justify-content-center mt-3 mx-0">
+      <div class="col-12 col-md-12 col-lg-12">
         <ul class="list-group overflow-auto shadow">
           <!-- v-for that loops through the items array, and lists them as an unordered list if there are items to list -->
           <li
@@ -38,8 +38,12 @@
           >
             {{ item }}
             <!-- button that when clicked removes targeted items from the list by using the removeItem function -->
-            <button v-on:click="removeItem(index)" type="button" class="btn btn-danger" id="removeItem">
-            
+            <button
+              v-on:click="removeItem(index)"
+              type="button"
+              class="btn btn-danger"
+              id="removeItem"
+            >
               <!-- the svg is the icon for the trashcan on the remove button -->
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -64,21 +68,22 @@
                     fill-rule="evenodd"
                     d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
                   />
-                </g></svg
-            ></button>
+                </g>
+              </svg>
+            </button>
           </li>
         </ul>
       </div>
     </div>
     <!-- row for the "show previous lists and save list" buttons -->
-    <div class="row mt-3 justify-content-center">
-      <div class="col-5">
-        <button type="button" class="btn btn-primary">
+    <div class="row my-3 justify-content-center mx-0">
+      <div class="col-6">
+        <button type="button" class="btn btn-primary btnHeight">
           Show previous grocery lists
         </button>
       </div>
-      <div class="col-5">
-        <button type="button" class="btn btn-success">
+      <div class="col-6">
+        <button type="button" class="btn btn-success btnHeight">
           save current grocery list
         </button>
       </div>
@@ -108,21 +113,19 @@ export default {
     //function that removes selected item from the list by using splice with index as identifier
     removeItem: function (index) {
       this.items.splice(index, 1);
-    }
-  }
-}
+    },
+
+    //need to add a filter function to prevent adding empty items to the list
+  },
+};
 </script>
 <!-- css styling for this component only -->
 <style scoped>
-.container {
-  margin: 0;
-  padding: 0;
-}
 /* styling for the list, also breaks long words automatically
    to prevent them going outside it's div */
 ul {
   width: 100%;
-  height: 461px;
+  height: 400px;
   list-style-type: none;
   padding: 0;
   word-wrap: break-word;
@@ -140,5 +143,8 @@ li:nth-child(even) {
 .list-group-item {
   text-align: left;
   font-size: 25px;
+}
+.btnHeight {
+  height: 100%;
 }
 </style>
